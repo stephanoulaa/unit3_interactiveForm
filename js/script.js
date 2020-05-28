18c4b01ebb18e09bab1456420c04bd22c4c2301b
Stephany Prodromos <stephprodromos@gmail.com>
2020-05-28 18:40:17 -0500

    added acitivities section

--- js/script.js
+++ js/script.js
@@ -19,11 +19,8 @@ selectTitle.addEventListener('change', (e) => {
 });
 
 // T-SHIRT SECTION:
-const selectThemeLabel = document.querySelector("label[for='design']");
-selectThemeLabel.innerHTML = "Please Select A T-Shirt Theme:"
 
 // define design menu section
-const selectColorLabel = document.querySelector("label[for='color']");
 const designSection = document.getElementById('design');
 const colorSection = document.getElementById('colors-js-puns');
 const colorOptions = document.getElementById('color').children;
@@ -33,26 +30,131 @@ colorSection.style.display = 'none';
 //choosePunOption.text = "Choose a JS Puns T-Shirt";
 //colorSection.appendChild(choosePunOption);
 
-
 // add event listener for when user chooses a design, it hides the other 3 that aren't relevant
 designSection.addEventListener('change', (e) => {
-    selectColorLabel.innerHTML = "Please Select A Color:"
     for (var i=0; i<colorOptions.length; i+=1) {
+        colorOptions[i].style.display = 'none';
         if (e.target.value === 'js puns') {
             // show first three colors
-            colorOptions[3].hidden = true;
-            colorOptions[4].hidden = true;
-            colorOptions[5].hidden = true;
             colorSection.style.display = '';
+            colorOptions[0].style.display = '';
+            colorOptions[1].style.display = '';
+            colorOptions[2].style.display = '';
         } else if (e.target.value === 'heart js') {
             // show last three colors
-            colorOptions[0].hidden = true;
-            colorOptions[1].hidden = true;
-            colorOptions[2].hidden = true;
+            colorOptions[3].style.display = '';
+            colorOptions[4].style.display = '';
+            colorOptions[5].style.display = '';
             colorSection.style.display = '';
-        }
+        } 
+//        else if (e.target === designSection.firstElementChild) {
+//            const selectThemeLabel = document.querySelector("label[for='design']");
+//            colorSection.style.display = '';
+//            selectThemeLabel.innerHTML = "Please Select A T-Shirt Theme:"
+//        }
     }
 });
+
+// REGISTER FOR ACTIVITIES SECTION:
+
+// total cost of checked activities
+let totalCost = 0;
+const totalCostLabel = document.createElement('label');
+
+// define activities section and locate all activities inputs
+const activities = document.querySelector('.activities');
+const listOfActivities = activities.querySelectorAll('input');
+//listOfActivities[i].attributes.getNamedItem('data-cost').value;
+
+//let chosenActivities = [];
+
+//// loop through all activities by PRICE
+//for (var i=0; i<listOfActivities.length; i+=1) {
+//    if (listOfActivities.checked) {
+////        chosenActivities.push(listOfActivities[i].attributes.getNamedItem('data-cost').value);
+////        console.log(chosenActivities);
+//        console.log("checked!");
+//    }
+//}
+
+// loop through all activity inputs and record their name, day/time, checked status and cost
+for (var i=0; i<listOfActivities.length; i+=1) {
+    listOfActivities[i].addEventListener('change', (e) => {
+        const activityChecked = e.target.checked;
+        const activityName = e.target.attributes.getNamedItem('name');
+        const dataCostVal = e.target.attributes.getNamedItem('data-cost');
+        const activityDate = e.target.attributes.getNamedItem('data-day-and-time');
+        // call addActivities() function here
+        addActivities(activityChecked, activityName, dataCostVal, activityDate);
+    })
+
+}
+
+
+// add conditionals: 
+//  when item is checked, add to the total and display on page
+//  when item is unchecked, subtract from the total and display on page
+//  when total is 0, DONT display on page
+
+function addActivities(activityChecked, activityName, dataCostVal, activityDate) {
+    if (activityChecked) {
+        totalCost += parseInt(dataCostVal.textContent);
+        displayTotalCost(totalCost);
+        // make sure we are not including first activity in the function
+        if (activityName.textContent !== "all") {
+          avoidOverlappingDates(activityDate, activityName);
+        }
+      } else {
+        // again need to ignore first activity
+        if (activityName.textContent !== "all") {
+          for (let i = 1; i < listOfActivities.length; i++) {
+            if (activityDate.textContent === listOfActivities[i].attributes.getNamedItem("data-day-and-time").textContent) {
+              listOfActivities[i].removeAttribute("disabled");
+            }
+          } //end of for loop
+        }
+        totalCost -= parseInt(dataCostVal.textContent);
+        displayTotalCost(totalCost);
+        if (totalCost === 0) {
+          activities.removeChild(totalCostLabel);
+        }
+    } // end of else
+}
+
+
+
+// use label element we created to dislay total cost, by appending below activities section
+function displayTotalCost() {
+    totalCostLabel.textContent = 'Total Cost: $' + totalCost;
+    activities.appendChild(totalCostLabel);
+}
+
+// create function that checks if the day/time of activities overlap by comparing name attribute 
+// if they do overlap, 'disabled' attribute is added so the user cannot choose it
+function avoidOverlappingDates(activityDate, activityName) {
+    // loop thru all dates (start with 1 because first activity is on all days)
+  for (let i = 1; i < listOfActivities.length; i++) {
+    if (activityName !== listOfActivities[i].attributes.getNamedItem("name")) {
+      if (activityDate.textContent === listOfActivities[i].attributes.getNamedItem("data-day-and-time").textContent) {
+        listOfActivities[i].setAttribute("disabled", "");
+      }
+    }
+  } // end of for loop
+};
+
+//console.log(listOfActivities.attributes.getNamedItem('data-cost').value);
+//const dataCostAttribute = listOfActivities.attributes.getAttribute('data-cost');
+//console.log(dataCostAttribute);
+
+
+
+
+
+
+
+
+
+
 
 
 
